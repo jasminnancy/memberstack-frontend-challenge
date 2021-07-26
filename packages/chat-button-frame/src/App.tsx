f3d391b353fdbbf7f9a777bca98083b8cf1062bf
Alexandria <alex.pugia816@hotmail.com>
2021-07-26 14:26:58 -0400

    completed challenge

--- packages/chat-button-frame/src/App.tsx
+++ packages/chat-button-frame/src/App.tsx
@@ -1,10 +1,62 @@
-import logo from './chat.png';
-import './App.css';
+import logo from "./chat.png";
+import "./App.css";
+import { useEffect, useMemo, useState } from "react";
+import { getCookie } from "../src/Helpers/utils";
 
 function App() {
+  let [color, setColor] = useState<string>(getCookie("color") || "#e66465");
+
+  //custom cookie event to check if cookies have changed
+  const cookieEvent = useMemo(
+    () =>
+      new CustomEvent("cookieChanged", {
+        bubbles: true,
+        detail: {
+          cookieValue: document.cookie,
+          checkChange: () => {
+            if (cookieEvent.detail.cookieValue !== document.cookie) {
+              cookieEvent.detail.cookieValue = document.cookie;
+              return 1;
+            } else {
+              return 0;
+            }
+          },
+          listenCheckChange: () => {
+            setInterval(() => {
+              if (cookieEvent.detail.checkChange() === 1) {
+                cookieEvent.detail.changed = true;
+                //fire the event
+                cookieEvent.target?.dispatchEvent(cookieEvent);
+              } else {
+                cookieEvent.detail.changed = false;
+              }
+            }, 10);
+          },
+          changed: false,
+        },
+      }),
+    []
+  );
+
+  //on load, adds custom cookie event
+  useEffect(() => {
+    document.body.dispatchEvent(cookieEvent);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  //listens for cookie changes
+  document.addEventListener("cookieChanged", (e: CustomEventInit) => {
+    //checks for changes to cookies
+    e.detail.listenCheckChange();
+    if (e.detail.changed === true) {
+      //set color to new cookie's color
+      setColor(getCookie("color"));
+    }
+  });
+
   return (
     <div className="App">
-      <div className="chat-icon">
+      <div className="chat-icon" style={{ backgroundColor: color }}>
         <img src={logo} className="chat-icon-image" alt="logo" />
       </div>
     </div>
--- packages/host/src/App.tsx
+++ packages/host/src/App.tsx
@@ -1,20 +1,25 @@
 import ColorPicker from "./components/ColorPicker";
 import ChatBotIFrame from "./components/ChatBotIFrame";
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { getCookie, setCookie } from "../src/components/Helpers/utils";
 
 const App = () => {
+  const [color, setColor] = useState<string>(getCookie("color") || "#e66465");
+
   const handleColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const color: string = e.target.value;
+    setCookie("color", color);
+    setColor(color);
   };
 
   return (
-    <>
-      <ColorPicker
-        value="#e66465"
-        onChange={handleColorChange}
-      />
+    <div className="container">
+      <h5 className="header">Change Chat Color: </h5>
+      <ColorPicker value={color} onChange={handleColorChange} />
       <ChatBotIFrame />
-    </>
+    </div>
   );
-}
+};
 
 export default App;
